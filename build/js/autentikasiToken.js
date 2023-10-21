6a44be3c5c85aa9ef6a5a7ba40c355c456e5e769
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-21 12:28:57 +0700

    push

--- build/js/autentikasiToken.js
+++ build/js/autentikasiToken.js
@@ -1,25 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Mendapatkan nilai cookie dengan nama "token" menggunakan fungsi getCookie
   const token = getCookie("token");
 
-  // Jika tidak ada token, redirect ke halaman signIn.html
+  // Jika tidak ada token, kembalikan ke halaman signIn.html
   if (!token) {
-    // Jika tidak ada token, tampilkan SweetAlert dan redirect ke halaman signIn.html
-    if (!token) {
-      Swal.fire({
-        icon: "warning",
-        title: "Oops...",
-        text: "Anda belum login!",
-        confirmButtonText: "OK",
-      }).then(() => {
-        window.location.href = "signIn.html";
-      });
-    }
-  }
-
-  // Fungsi untuk mendapatkan nilai cookie berdasarkan nama
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
+    Swal.fire({
+      icon: "info",
+      title: "Informasi",
+      text: "Anda belum login. Kembali ke halaman sign in.",
+      confirmButtonText: "OK",
+    }).then(() => {
+      // Redirect ke halaman sign in
+      window.location.href = "../signIn.html";
+    });
   }
 });
